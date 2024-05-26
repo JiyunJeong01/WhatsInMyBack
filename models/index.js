5909dd65a7768f41a553eb3a7014dd4c982693eb
JeongBin Choi <20210900@sungshin.ac.kr>
2024-05-26 11:07:15 +0900

    to merge profile

--- models/index.js
+++ models/index.js
@@ -2,5 +2,6 @@ const db ={};
 
 db.comment = require("./comment.js")
 db.member = require("./member.js")
+db.preference = require("./preference.js")
 
 module.exports = db;
--- models/member.js
+++ models/member.js
@@ -39,3 +39,50 @@ exports.deleteMember = async (userId) => {
         console.error("쿼리 실행 중 오류:", error);
     };
 }
+
+exports.loadMember = async (userId) => {
+    try {
+        const db = await require('../main').connection();
+        let [rows] = await db.query('SELECT username, nickname, email, age, gender, job, follow_notification, like_notification, comment_notification, recommend_notification, picture_base64, bio FROM member WHERE member_id = ?', [userId]);
+
+        return (rows)
+    }
+    catch (error) {
+        console.error("쿼리 실행 중 오류:", error);
+    };
+}
+
+exports.updateMember = async (userId, username, nickname, email, age, gender, job, follow_notification, like_notification, comment_notification, recommend_notification, picture_base64, bio) => {
+    try {
+        const db = await require('../main').connection();
+        const [result] = await db.execute('UPDATE member SET username = ?, nickname = ?, email = ?, age = ?, gender = ?, job = ?, follow_notification = ?, like_notification = ?, comment_notification = ?, recommend_notification = ?, picture_base64 = ?, bio = ? WHERE member_id = ?',
+            [username, nickname, email, age, gender, job, follow_notification, like_notification, comment_notification, recommend_notification, picture_base64, bio, userId]);
+
+        return (result.affectedRows===1);
+    }
+    catch (error) {
+        console.error("쿼리 실행 중 오류:", error);
+    };
+}
+
+exports.getUserByEmail = async (email) => {
+    try {
+        const db = await require('../main').connection();
+        const [rows, fields] = await db.query('SELECT * FROM member WHERE email = ?', [email]);
+
+        return rows.length ? rows[0] : null;
+    } catch (error) {
+        console.error("쿼리 실행 중 오류:", error);
+    };
+}
+
+exports.getUserByNickname = async (nickname) => {
+    try {
+        const db = await require('../main').connection();
+        const [rows, fields] = await db.query('SELECT * FROM member WHERE nickname = ?', [nickname]);
+
+        return rows.length ? rows[0] : null;
+    } catch (error) {
+        console.error("쿼리 실행 중 오류:", error);
+    };
+}
